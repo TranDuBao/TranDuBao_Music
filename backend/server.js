c4dab28fc8fa6837f598a06fd40cbae73472126f
TranDuBao <dubao1005@gmail.com>
2026-08-09 19:32:32 +0700

    Allow dynamic origin in CORS for Docker deployment

--- backend/server.js
+++ backend/server.js
@@ -35,11 +35,7 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: [
-    process.env.FRONTEND_URL || 'http://localhost:5173',
-    'http://localhost:5174',
-    'http://localhost:5175',
-  ],
+  origin: true,
   credentials: true,
 }));
 
